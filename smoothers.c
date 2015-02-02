cd45fc59a6a8698098f76fc849523b3dda220614
Francisco Sanz Garcia <francisco.sanz.g@gmail.com>
2015-02-02 13:43:50 +0100

    working in smoothers

--- smoothers.c
+++ smoothers.c
@@ -25,12 +25,48 @@
  * At this moment I don't have very clear anything
  */
 void smooth_1(triangle *** mgrid, int level){
+  int i,j,l,m;
+  double temp0 = 0.0;
+  double temp1 = 0.0;
+  double temp2 = 0.0;
+
   for(i=1;i<pow(2,level)-1;i++){ // For all files, interior points
     for(j=1;j<pow(2,level-i)-1;j++){ // For all columns
-        mgrid[level][i][j].U[0]=oper[0][0].op[3]*mgrid[level][i][j-1]+
-                                oper[0][0].op[4]*mgrid[level][i][j]+
-                                oper[0][0].op[5]*mgrid[level][i][j+1];
-    }
+      /* Smoothing edge u */
+      for(l=0;l<3;l++){
+        for(m=0;m<3;m++){
+          temp0=temp0+oper[0][uu].op[3*l+m]*mgrid[level][i+1-l][j-1+m].function_u[edge_u];
+          temp1=temp1+oper[0][uv].op[3*l+m]*mgrid[level][i+1-l][j-1+m].function_u[edge_v];
+          temp2=temp2+oper[0][uw].op[3*l+m]*mgrid[level][i+1-l][j-1+m].function_u[edge_w];
+        }
+      }
+      mgrid[level][i][j].function_u[edge_u]+temp0+temp1+temp2;
+      temp0=0.0;
+      temp1=0.0;
+      temp2=0.0;
+
+      for(l=0;l<3;l++){
+        for(j=0;j<3;j++){
+          temp0=temp0+oper[0][vu].op[3*l+m]*mgrid[level][i+1-l][j-1+m].function_u[edge_u];
+          temp1=temp1+oper[0][vv].op[3*l+m]*mgrid[level][i+1-l][j-1+m].function_u[edge_v];
+          temp2=temp2+oper[0][vw].op[3*l+m]*mgrid[level][i+1-l][j-1+m].function_u[edge_w];
+        }
+      }
+      mgrid[level][i][j].function_u[edge_v]+temp0+temp1+temp2;
+
+      /* Smoothing edge w */
+      mgrid[level][i][j].function_u[edge_w]=
+        oper[0][wu].op[0]*mgrid[level][i+1][j-1].function_u[edge_u]+
+        oper[0][wu].op[1]*mgrid[level][i+1][j  ].function_u[edge_u]+
+        oper[0][wu].op[3]*mgrid[level][i  ][j-1].function_u[edge_u]+
+        oper[0][wu].op[4]*mgrid[level][i  ][j  ].function_u[edge_u]+
+          oper[0][wv].op[0]*mgrid[level][i+1][j-1].function_u[edge_v]+
+          oper[0][wv].op[3]*mgrid[level][i  ][j-1].function_u[edge_v]+
+          oper[0][wv].op[4]*mgrid[level][i  ][j  ].function_u[edge_v]+
+          oper[0][wv].op[7]*mgrid[level][i-1][j  ].function_u[edge_v]+
+            oper[0][ww].op[1]*mgrid[level][i+1][j  ].function_u[edge_w]+
+            oper[0][ww].op[4]*mgrid[level][i  ][j  ].function_u[edge_w]+
+            oper[0][ww].op[7]*mgrid[level][i-1][j  ].function_u[edge_w];
   }
 }
 
