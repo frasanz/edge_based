bab4b629af446b32fa3a7eb111cad31df857e017
spac <frasanz@bifi.es>
2015-02-28 17:41:48 +0100

    finished interpolation

--- interpolation.c
+++ interpolation.c
@@ -23,17 +23,31 @@ void interpolate_one(triangle*** mgrid, int level_inf){
   int i,j;
   int I,J;
   int level_sup=level_inf+1;
-  for(i=1;i<pow(2,level_inf);i++){
+  for(i=0;i<pow(2,level_inf);i++){
     I=2*i;
     for(j=0;j<pow(2,level_inf)-i;j++){
       J=2*j;
-      mgrid[level_sup][I  ][J  ].function_u[edge_u]=
-      mgrid[level_sup][I  ][J+1].function_u[edge_u]=
-      mgrid[level_sup][I+1][J  ].function_u[edge_u]=
-      mgrid[level_inf][i][j].function_u[edge_u];
+      /*** edge_u ***/
+      mgrid[level_sup][I  ][J  ].function_u[edge_u]=1.0*i+0.1*j;
+      mgrid[level_sup][I  ][J+1].function_u[edge_u]=1.0*i+0.1*j;
+      mgrid[level_sup][I+1][J  ].function_u[edge_u]=1.0*i+0.1*j;
+      if(i>0)
+        mgrid[level_sup][I-1][J+1].function_u[edge_u]=1.0*i+0.1*j;
+      /*** edge_v ***/
+      mgrid[level_sup][I  ][J  ].function_u[edge_v]=1.0*i+0.1*j;
+      mgrid[level_sup][I  ][J+1].function_u[edge_v]=1.0*i+0.1*j;
+      mgrid[level_sup][I+1][J  ].function_u[edge_v]=1.0*i+0.1*j;
+      if(j<pow(2,level_inf)-i-1) // Not in the diag
+        mgrid[level_sup][I+1][J+1].function_u[edge_v]=1.0*i+0.1*j;
+      /*** edge_w ***/
+      mgrid[level_sup][I  ][J  ].function_u[edge_w]=1.0*i+0.1*j;
+      mgrid[level_sup][I  ][J+1].function_u[edge_w]=1.0*i+0.1*j;
+      mgrid[level_sup][I+1][J  ].function_u[edge_w]=1.0*i+0.1*j;
+      if(j>0)
+        mgrid[level_sup][I+1][J-1].function_u[edge_w]=1.0*i+0.1*j;
+
+
     }
   }
-
-
 }
 
